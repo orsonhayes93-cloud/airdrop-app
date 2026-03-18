6a1270e152930ce99179c1b9c088d8a6ecd4212c
orsonhayes93-cloud <orsonhayes93@gmail.com>
2026-03-18 12:02:59 +0000

    Added dynamic /hub/:id route for the Hub page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,11 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/home";
-import SwapPage from "@/pages/swap";
-import StakePage from "@/pages/stake";
-import AirdropPage from "@/pages/airdrop";
+import HubPage from './HubPage';
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/swap" component={SwapPage} />
-      <Route path="/stake" component={StakePage} />
-      <Route path="/airdrop" component={AirdropPage} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+// ... other imports
 
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
-
-export default App;
+<Routes>
+  <Route path="/swap" component={SwapPage} />
+  <Route path="/stake" component={StakePage} />
+  <Route path="/airdrop" component={AirdropPage} />
+  <Route path="/hub/:id" component={HubPage} />
+  <Route component={NotFound} />
+</Routes>
